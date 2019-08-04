d6be85dd95a9fe39bb4c9d6a92a599a2abac92af
Aman Sharma <writetoaman99@gmail.com>
2019-08-05 00:18:03 +0530

    Added api petfinder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 class App extends React.Component {
-  handelHeadingClick() {
-    alert("ouuchhhh!!");
+  componentDidMount()
+  {
+    const promise=petfinder.breed.list({animal:"dog"});
+
+    promise.then(console.log,console.error)
   }
   render() {
-    return(
+    return (
       <div>
-        <h1 >Adopt Me!!!</h1>
-        <Pet name="Aman Sharma" status="Second Year" college="Maharaja Agrasen Institute Of Technology" />
-        <Pet name="Pulkit Gupta" status="Third Year" college="Maharaha Surajmal Institute Of Technology" />
-        <Pet name="Rohan Garg" status="Fourth Year" college="Bhagwan Pashuram Institute Of Technology" />
-
+        <h1>Adopt Me!!!</h1>
+        <Pet
+          name="Aman Sharma"
+          status="Second Year"
+          college="Maharaja Agrasen Institute Of Technology"
+        />
+        <Pet
+          name="Pulkit Gupta"
+          status="Third Year"
+          college="Maharaha Surajmal Institute Of Technology"
+        />
+        <Pet
+          name="Rohan Garg"
+          status="Fourth Year"
+          college="Bhagwan Pashuram Institute Of Technology"
+        />
       </div>
-    )
+    );
   }
 }
 render(React.createElement(App), document.getElementById("root"));
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,6 @@
 import React from 'react';
 const Pet = props => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, props.name),
-  //   React.createElement("h1", {}, props.status),
-  //   React.createElement("h1", {}, props.college)
-  // ]);
+
   return(
     <div>
       <h1>{props.name}</h1>
